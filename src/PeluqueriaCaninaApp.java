6f6a1f0df09328f6a80bfabdcb54816e9c000767
ISP63-4 <heber96nr@gmail.com>
2025-05-22 20:58:41 -0300

    clase 22/05

--- src/PeluqueriaCaninaApp.java
+++ src/PeluqueriaCaninaApp.java
@@ -1,29 +1,44 @@
 import model.*;
 
+import java.util.Scanner;
 
 public class PeluqueriaCaninaApp {
 
   public static void main(String[] args) {
-    // Crear mascotas
-    Mascota mascota1 = new Mascota("Varian", "Naranja", EspecieMascota.GATO, 2, "Heber");
-    Mascota mascota2 = new Mascota("Oliver", "Cruza calle con vereda", EspecieMascota.PERRO, 1, "Lourdes");
-    Mascota mascota3 = new Mascota("Lobo", "Labrador", EspecieMascota.PERRO, 3, "Lourdes");
+   String[] mascotas = new String[2];
+   String[] turnos = new String[10];
+   Scanner sc = new Scanner(System.in);
 
-    // Crear servicios
-    Servicio servicio1 = new Servicio(TipoServicio.DESPARASITACION, "Desparasitación de pulgas y garrapatas", 30, 5000);
-    Servicio servicio2 = new Servicio(TipoServicio.BAÑO, "Baño y corte de pelo", 60, 10000);
-    Servicio servicio3 = new Servicio(TipoServicio.PELUQUERIA, "Corte de pelo y peinado", 45, 8000);
+   /* public Mascota(String nombre, String raza,
+                 EspecieMascota especieMascota, int edad, String nombreDuenio) */
 
-    // Crear turno
-    Turno turno1 = new Turno("2025-05-20", "09:00", EstadoServicio.REALIZADO, mascota3, servicio3);
-    Turno turno2 = new Turno("2025-05-21", "10:00", EstadoServicio.PROGRAMADO, mascota1, servicio1);
-    Turno turno3 = new Turno("2025-05-22", "10:30", EstadoServicio.PROGRAMADO, mascota2, servicio2);
+   do {
+     EspecieMascota especie = null;
+     for (int i = 0; i < mascotas.length; i++){
+       System.out.println("Ingrese el nombre de la mascota: ");
+       String nombreAnimal = sc.nextLine();
+       System.out.println("Ingrese la raza de la mascota: ");
+       String razaAnimal = sc.nextLine();
+       System.out.println("Ingrese la especie de la mascota: ");
+       String especieAnimal = sc.nextLine();
+       if (especieAnimal.equalsIgnoreCase("perro")) {
+         especie = EspecieMascota.PERRO;
+       } else if (especieAnimal.equalsIgnoreCase("gato")) {
+         especie = EspecieMascota.GATO;
+       } else {
+         especie = EspecieMascota.VACA;
+       }
+       System.out.println("Ingrese la edad del animal: ");
+       int edadAnimal = sc.nextInt();
+       sc.nextLine();
+       System.out.println("Ingrese el nombre del dueño de la mascota: ");
+       String duenioMascota = sc.nextLine();
 
-    // Mostrar datos de las mascotas
-    System.out.println(turno1.mostrarDatos());
-    System.out.println("-------------------------");
-    System.out.println(turno2.mostrarDatos());
-    System.out.println("-------------------------");
-    System.out.println(turno3.mostrarDatos());
+       Mascota mascota = new Mascota(nombreAnimal, razaAnimal, especie , edadAnimal, duenioMascota);
+       System.out.println(mascota.mostrarDatos());
+     }
+   } while(mascotas.length < 2 );
+
+
   }
 }
